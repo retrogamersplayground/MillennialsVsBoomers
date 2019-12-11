9748669be522e0b89374897e9902e89e9e5dd2e6
Neil Floyd <neilfloyd99@gmail.com>
2019-12-10 21:55:00 -0500

    add score

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -10,13 +10,14 @@
         <img src="@/assets/millennial.png" alt="millennial" />
       </div>
       <div class="gameDiv">
+        <div class="score">Score: {{ newScore }}</div>
         <ul>
           <li v-for="question in questionBank" class="questionsLi" v-bind:value="question" v-bind:key="question">{{ question[0] }}
             <ul>
-              <li><button class="answerButton">{{ question[1] }}</button></li>
-              <li><button class="answerButton">{{ question[2][0] }}</button></li>
-              <li><button class="answerButton">{{ question[2][1] }}</button></li>
-              <li><button class="answerButton">{{ question[2][2] }}</button></li>
+              <li><button class="answerButton" id="button1" v-on:click="increaseScore()">{{ question[1] }}</button></li>
+              <li><button class="answerButton" id="button2" v-on:click="increaseScore2()">{{ question[2][0] }}</button></li>
+              <li><button class="answerButton" id="button3" v-on:click="increaseScore3()">{{ question[2][1] }}</button></li>
+              <li><button class="answerButton" id="button4" v-on:click="increaseScore4()">{{ question[2][2] }}</button></li>
             </ul>
           </li>
         </ul>
@@ -33,18 +34,21 @@ export default {
     return {
       wholeResponses: [],
       results: [],
-      questionBank: []
+      questionBank: [],
+      score: 0,
+      newScore: localStorage.getItem('score'),
+      correct_answer: []
     }
   },
   mounted () {
     axios
-      .get('https://opentdb.com/api.php?amount=10')
+      .get('https://opentdb.com/api.php?amount=1&type=multiple')
       .then(response => {
         this.wholeResponses = response.data
         this.results = this.wholeResponses.results
         for (const result of this.results) {
           this.questionBank.push([result.question, result.correct_answer, result.incorrect_answers])
-          console.log(this.questionBank[0][1])
+          this.correct_answer.push([result.correct_answer])
         }
       })
       .catch(error => {
@@ -52,6 +56,60 @@ export default {
       })
   },
   methods: {
+    increaseScore () {
+      let button1 = document.getElementById('button1').innerHTML
+      console.log(button1)
+      console.log('****' + this.correct_answer[0][0])
+      if (button1 === this.correct_answer[0][0]) {
+        this.score = this.score + 1
+        this.score = localStorage.setItem('score', this.score)
+        alert('Correct Answer!')
+        location.reload()
+      } else {
+        alert('Nope! Sorry Snowflake!')
+        location.reload()
+      }
+      this.score = this.newScore
+    },
+    increaseScore2 () {
+      let button2 = document.getElementById('button2').innerHTML
+      console.log(button2)
+      console.log('****' + this.correct_answer[0][0])
+      if (button2 === this.correct_answer) {
+        this.score = this.score + 1
+        alert('Correct Answer!')
+        location.reload()
+      } else {
+        alert('Nope! Sorry Snowflake!')
+        location.reload()
+      }
+    },
+    increaseScore3 () {
+      let button3 = document.getElementById('button3').innerHTML
+      console.log(button3)
+      console.log('****' + this.correct_answer[0][0])
+      if (button3 === this.correct_answer) {
+        this.score = this.score + 1
+        alert('Correct Answer!')
+        location.reload()
+      } else {
+        alert('Nope! Sorry Snowflake!')
+        location.reload()
+      }
+    },
+    increaseScore4 () {
+      let button4 = document.getElementById('button4').innerHTML
+      console.log(button4)
+      console.log('****' + this.correct_answer[0][0])
+      if (button4 === this.correct_answer) {
+        this.score = this.score + 1
+        alert('Correct Answer!')
+        location.reload()
+      } else {
+        alert('Nope! Sorry Snowflake!')
+        location.reload()
+      }
+    }
   },
   components: {
     'app-navigation': Navigation
@@ -111,5 +169,8 @@ export default {
 .answerButton:hover {
     background-color: #6775b6;
     color: #ffffff;
+}
+.score {
+  font-size: 32px;
 }
 </style>
