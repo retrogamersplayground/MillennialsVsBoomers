e2813f38f94adc90d4f6b63203d060bf74d39e9b
Neil Floyd <neilfloyd99@gmail.com>
2019-12-31 21:22:46 -0500

    firebaseInit.js and  firebaseConfig.js added, Login.vue, Register.vue and ResetPassword.vue added, updat ed main.js, added nav Guards to index.js

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,123 +1,3 @@
-<!--
-<template>
-  <div class="game">
-    <app-navigation></app-navigation>
-    <div class="showcaseDiv">
-      <h2>Let's Play!</h2>
-    </div>
-    <div class="gameImages">
-      <h2 class="millennialFont">Millennial</h2>
-      <div class="gameImage">
-        <img src="@/assets/millennial.png" alt="millennial" />
-      </div>
-      <div class="gameDiv">
-        <div class="score">Score: {{ newScore }}</div>
-        <ul>
-          <li v-for="question in questionBank" class="questionsLi" v-bind:value="question" v-bind:key="question">{{ question[0] }}
-            <ul>
-              <li><button class="answerButton" id="button1" v-on:click="increaseScore()">{{ question[1] }}</button></li>
-              <li><button class="answerButton" id="button2" v-on:click="increaseScore2()">{{ question[2][0] }}</button></li>
-              <li><button class="answerButton" id="button3" v-on:click="increaseScore3()">{{ question[2][1] }}</button></li>
-              <li><button class="answerButton" id="button4" v-on:click="increaseScore4()">{{ question[2][2] }}</button></li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-</template>
-
-<script>
-import Navigation from './Navigation.vue'
-import axios from 'axios'
-export default {
-  data () {
-    return {
-      wholeResponses: [],
-      results: [],
-      questionBank: [],
-      score: 0,
-      newScore: localStorage.getItem('score'),
-      correct_answer: []
-    }
-  },
-  mounted () {
-    axios
-      .get('https://opentdb.com/api.php?amount=1&type=multiple')
-      .then(response => {
-        this.wholeResponses = response.data
-        this.results = this.wholeResponses.results
-        for (const result of this.results) {
-          this.questionBank.push([result.question, result.correct_answer, result.incorrect_answers])
-          this.correct_answer.push([result.correct_answer])
-        }
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  },
-  methods: {
-    increaseScore () {
-      let button1 = document.getElementById('button1').innerHTML
-      console.log(button1)
-      console.log('****' + this.correct_answer[0][0])
-      if (button1 === this.correct_answer[0][0]) {
-        this.score = this.score + 1
-        this.score = localStorage.setItem('score', this.score)
-        alert('Correct Answer!')
-        location.reload()
-      } else {
-        alert('Nope! Sorry Snowflake!')
-        location.reload()
-      }
-      this.score = this.newScore
-    },
-    increaseScore2 () {
-      let button2 = document.getElementById('button2').innerHTML
-      console.log(button2)
-      console.log('****' + this.correct_answer[0][0])
-      if (button2 === this.correct_answer) {
-        this.score = this.score + 1
-        alert('Correct Answer!')
-        location.reload()
-      } else {
-        alert('Nope! Sorry Snowflake!')
-        location.reload()
-      }
-    },
-    increaseScore3 () {
-      let button3 = document.getElementById('button3').innerHTML
-      console.log(button3)
-      console.log('****' + this.correct_answer[0][0])
-      if (button3 === this.correct_answer) {
-        this.score = this.score + 1
-        alert('Correct Answer!')
-        location.reload()
-      } else {
-        alert('Nope! Sorry Snowflake!')
-        location.reload()
-      }
-    },
-    increaseScore4 () {
-      let button4 = document.getElementById('button4').innerHTML
-      console.log(button4)
-      console.log('****' + this.correct_answer[0][0])
-      if (button4 === this.correct_answer) {
-        this.score = this.score + 1
-        alert('Correct Answer!')
-        location.reload()
-      } else {
-        alert('Nope! Sorry Snowflake!')
-        location.reload()
-      }
-    }
-  },
-  components: {
-    'app-navigation': Navigation
-  }
-}
-</script>
--->
 <template>
   <div class="game">
     <app-navigation></app-navigation>
@@ -132,7 +12,7 @@ export default {
       <div class="gameDiv">
         <div class="gameCount">Question: {{ gameCount }}</div>
         <div class="score">Score: {{ score }}</div>
-        <ul>
+        <ul v-if="!gameOver">
           <li v-html="currentQuestionText">{{ currentQuestionText }}</li>
             <li
               v-html="answer"
@@ -143,6 +23,10 @@ export default {
             >
               {{ answer }}
             </li>
+        </ul>
+        <ul v-if="gameOver">
+          <img src="@/assets/temgtriggered.gif" alt="triggered Canadian">
+          <!--<video src="@/assets/triggered1.mp4" autoplay loop></video>-->
         </ul>
       </div>
     </div>
@@ -164,7 +48,8 @@ export default {
       currentQuestionText: '',
       gameCount: 1,
       answerArray: [],
-      shuffledAnswers: []
+      shuffledAnswers: [],
+      gameOver: false
     }
   },
   async mounted () {
@@ -222,6 +107,7 @@ export default {
         await this.getQuestion()
       } else {
         alert('GameOver')
+        this.gameOver = true
       }
     }
   },
